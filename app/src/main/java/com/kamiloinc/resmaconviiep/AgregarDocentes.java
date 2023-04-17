0a18f3d198ac7f21f03009f27907cc4cbac49753
Kamilo Gomez <fotocopiasa100@gmail.com>
2023-04-17 09:51:37 -0500

    arreglos perfil e agregar class docentes

---
+++ app/src/main/java/com/kamiloinc/resmaconviiep/AgregarDocentes.java
@@ -0,0 +1,14 @@
+package com.kamiloinc.resmaconviiep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarDocentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_docentes);
+    }
+}
--- app/src/main/java/com/kamiloinc/resmaconviiep/Inicio.java
+++ app/src/main/java/com/kamiloinc/resmaconviiep/Inicio.java
@@ -6,10 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Inicio extends AppCompatActivity {
+
+    Button agregarDocente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,24 @@ public class Inicio extends AppCompatActivity {
         setContentView(R.layout.activity_inicio);
 
         referenciar2();
+        conexiones();
+
+    }
+
+    private void conexiones() {
+
+        agregarDocente.findViewById(R.id.btn_agregarDocente);
+
+        agregarDocente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent( Inicio.this, AgregarDocentes.class );
+                startActivity(intent);
+
+            }
+        });
+
     }
 
 
