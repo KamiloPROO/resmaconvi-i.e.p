f33f29da9d4cc545611f483a739347e540fe185e
Kamilo Gomez <fotocopiasa100@gmail.com>
2023-05-02 18:05:11 -0500

    cambios formulario registrar docente

--- app/src/main/java/com/kamiloinc/resmaconviiep/AgregarDocentes.java
+++ app/src/main/java/com/kamiloinc/resmaconviiep/AgregarDocentes.java
@@ -9,9 +9,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -21,12 +24,14 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.ktx.Firebase;
 
+import java.security.PublicKey;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class AgregarDocentes extends AppCompatActivity {
 
+    String generoSpinner;
     Button agregarDocentesFirebase;
 
     EditText nombre, numDocumento, fechaNacimiento, genero, cargo, sede, especialidad, nivelEnsenianza, recidencia, numTelefono, correo;
@@ -35,7 +40,13 @@ public class AgregarDocentes extends AppCompatActivity {
 
     ProgressDialog pd;
 
+    Spinner spinnerGenero;
 
+
+
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,10 +54,12 @@ public class AgregarDocentes extends AppCompatActivity {
 
         agregarDocentesFirebase = findViewById(R.id.btnAgregarDocenteForm);
 
+        spinnerGenero = findViewById(R.id.idSpinnerGenero);
+
         nombre = findViewById(R.id.editNombreDocente);
         numDocumento = findViewById(R.id.editNumDocuDocente);
         fechaNacimiento = findViewById(R.id.editFechaNacimiento);
-        genero = findViewById(R.id.editGeneroDocente);
+        //genero = findViewById(R.id.editGeneroDocente);
         cargo = findViewById(R.id.editCargoDocente);
         sede = findViewById(R.id.editSedeDocente);
         especialidad = findViewById(R.id.editEspecialidadDocente);
@@ -67,14 +80,35 @@ public class AgregarDocentes extends AppCompatActivity {
 
     private void referenciar() {
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Generos, android.R.layout.simple_spinner_item);
+
+        spinnerGenero.setAdapter(adapter);
+
+        spinnerGenero.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+               generoSpinner = adapterView.getItemAtPosition(i).toString();
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         agregarDocentesFirebase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+
+
                 String nombreDocente = nombre.getText().toString().trim();
                 String numDocumentoDocente = numDocumento.getText().toString().trim();
                 String fechaNacimientoDocente = fechaNacimiento.getText().toString().trim();
-                String generoDocente = genero.getText().toString().trim();
+                String generoDocente = generoSpinner.trim();
                 String cargoDocente = cargo.getText().toString().trim();
                 String sedeDocente = sede.getText().toString().trim();
                 String especialidadDocente = especialidad.getText().toString().trim();
@@ -83,8 +117,7 @@ public class AgregarDocentes extends AppCompatActivity {
                 String numTelefonoDocente = numTelefono.getText().toString().trim();
                 String correoDocente = correo.getText().toString().trim();
 
-                if (nombreDocente.isEmpty() && numDocumentoDocente.isEmpty() && fechaNacimientoDocente.isEmpty() && generoDocente.isEmpty() && cargoDocente.isEmpty() && sedeDocente.isEmpty()
-                        && especialidadDocente.isEmpty() && nivelEnsenianzaDocente.isEmpty() && recidenciaDocente.isEmpty() && numTelefonoDocente.isEmpty() && correoDocente.isEmpty() ){
+                if (nombreDocente.isEmpty() && numDocumentoDocente.isEmpty() && fechaNacimientoDocente.isEmpty() && generoDocente.isEmpty() && cargoDocente.isEmpty() && sedeDocente.isEmpty() && especialidadDocente.isEmpty() && nivelEnsenianzaDocente.isEmpty() && recidenciaDocente.isEmpty() && numTelefonoDocente.isEmpty() && correoDocente.isEmpty() ){
 
                     //Toast.makeText(AgregarDocentes.this, "Por favor ingresa todos los datos para poder continuar...", Toast.LENGTH_SHORT).show();
 
@@ -111,11 +144,12 @@ public class AgregarDocentes extends AppCompatActivity {
 
         Map<String, Object> map = new HashMap<>();
 
+
         map.put("id", id);
         map.put("nombre", nombreDocente);
         map.put("numDocumento", numDocumentoDocente);
         map.put("fechaNacimiento", fechaNacimientoDocente);
-        map.put("genero", generoDocente);
+        map.put("genero",generoDocente);
         map.put("cargo", cargoDocente);
         map.put("sede", sedeDocente);
         map.put("especialidad", especialidadDocente);
@@ -126,26 +160,26 @@ public class AgregarDocentes extends AppCompatActivity {
 
 
 
-      firebaseFirestore.collection("Docentes").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-          @Override
-          public void onSuccess(DocumentReference documentReference) {
+        firebaseFirestore.collection("Docentes").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
 
-              pd.dismiss();
+                pd.dismiss();
 
-              Toast.makeText(AgregarDocentes.this, "Docente Agregado Correctamente", Toast.LENGTH_SHORT).show();
+                Toast.makeText(AgregarDocentes.this, "Docente Agregado Correctamente", Toast.LENGTH_SHORT).show();
 
-          }
-      }).addOnFailureListener(new OnFailureListener() {
-          @Override
-          public void onFailure(@NonNull Exception e) {
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
 
-              pd.dismiss();
+                pd.dismiss();
 
-              Toast.makeText(AgregarDocentes.this, "Error Al Agregar Docente", Toast.LENGTH_SHORT).show();
+                Toast.makeText(AgregarDocentes.this, "Error Al Agregar Docente", Toast.LENGTH_SHORT).show();
 
 
-          }
-      });
+            }
+        });
 
     }
 
@@ -154,7 +188,7 @@ public class AgregarDocentes extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
 
-       bottomNavigationView.setSelectedItemId(R.id.inicio);
+        bottomNavigationView.setSelectedItemId(R.id.inicio);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
